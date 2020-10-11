b05b856a26d64fc63d9c71ba8e35b65ffbc285e3
afrikpaysas <58859378+afrikpaysas@users.noreply.github.com>
2020-10-11 14:57:51 +0100

    Update class-wc-gateway-expressunion-payments.php

--- includes/class-wc-gateway-expressunion-payments.php
+++ includes/class-wc-gateway-expressunion-payments.php
@@ -175,7 +175,7 @@ class WC_Gateway_EU extends WC_Payment_Gateway {
 	 * @return bool
 	 */
 	public function is_valid_for_use() {
-		return in_array( get_woocommerce_currency(), apply_filters( 'woocommerce_expressunion_supported_currencies', array( 'XAF','USD','EUR' ) ) );
+		return in_array( get_woocommerce_currency(), apply_filters( 'woocommerce_expressunion_supported_currencies', array( 'XAF','USD','EUR','CFA' ) ) );
 	}
 
 	/**
